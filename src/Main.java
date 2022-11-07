16105419ac6bc0688036f1caf9cf84457b12d81f
JoshMinnich <minnichjs21@gcc.edu>
2022-11-07 13:48:00 -0500

    Second edition commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = scn.next();
+        System.out.println("Hello " + name);
+    }
 }
